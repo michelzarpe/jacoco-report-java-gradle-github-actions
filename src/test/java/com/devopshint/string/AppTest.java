2f80f67d758a5d6d91f65444a954498a029a5cab
DevOpsHint <83763090+devopshint@users.noreply.github.com>
2023-03-09 19:19:14 +0530

    Update AppTest.java

--- src/test/java/com/devopshint/string/AppTest.java
+++ src/test/java/com/devopshint/string/AppTest.java
@@ -2,7 +2,6 @@ package com.devopshint.string;
 
 public class AppTest {
 
-	@Test
 	public void verifySomeRandomMethod() {
 		AppTest junitTest = new AppTest();
 		junitTest.someRandomMethod(11, 12);
