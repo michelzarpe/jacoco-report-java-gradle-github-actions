361758b7f892149bfe4a9ddb7feef57ca6897ee2
DevOpsHint <83763090+devopshint@users.noreply.github.com>
2023-03-09 18:58:31 +0530

    Update AppTest.java

--- src/test/java/com/devopshint/string/AppTest.java
+++ src/test/java/com/devopshint/string/AppTest.java
@@ -1,28 +1,10 @@
-package com.devopshint.string;
-
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
 public class AppTest {
 
-	String input1 = "noon";
-	App app = new App();
-	boolean expected = true;
-
 	@Test
-	public void isPlaindromeTest() {
-		assertEquals(expected, app.isPalindrome(input1));
+	public void verifySomeRandomMethod() {
+		Example junitTest = new Example();
+		junitTest.someRandomMethod(11, 12);
 	}
-
-	@Test
-	public void isNotPlaindromeTest() {
-		assertEquals(false, app.isPalindrome("abc"));
-	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void isNotPlaindromeExceptionTest() {
-		assertEquals(false, app.isPalindrome(null));
-	}
-
 }
